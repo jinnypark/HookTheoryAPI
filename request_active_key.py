fdc4ca998bd72d4e0f677687a1fca64835137a23
Jinny Park <jinny.park.mus@gmail.com>
2019-11-13 04:49:39 -0500

    Update request_active_key.py

--- request_active_key.py
+++ request_active_key.py
@@ -2,11 +2,12 @@
 # A python script for requesting API
 # Python 3.6
 import requests, json #conda install request
-#from aiohttp import web
-header = {"Accept": "application/json", "Content-Type": "application/json", "username": "jinnyparkmus", "password": "mypassword"} #replace "mypassword" with real pw
+header = {"Accept": "application/json", "Content-Type": "application/json", "username": "myid", "password": "mypassword"} 
+#replace "myid" and "mypassword" with your own
 
 url = 'https://api.hooktheory.com/v1/users/auth'
 r = requests.post(url, data=header)
 print(r.json())
 
-# output: {'id': 012345, 'username': 'jinnyparkmus', 'email': 'youremail@gmail.com', 'activkey': 'abcdefgghjqwert1234556', 'plus': False}
+# output: {'id': 012345, 'username': 'yourid', 'email': 'youremail@gmail.com', 'activkey': 'abcdefgghjqwert1234556', 'plus': False}
+# use 'activekey' value to access data
